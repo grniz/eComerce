0cfea06397e2929c38340c097d376344a0c5a5e8
grniz <gr.niz89@gmail.com>
2023-12-04 23:00:31 -0300

    proyecto final

--- src/config/config.js
+++ src/config/config.js
@@ -6,16 +6,7 @@ export default {
   app: {
     ENV: process.env.NODE_ENV || "production",
   },
-  mailing: {
-    SERVICE: process.env.MAILING_SERVICE,
-    USER: process.env.MAILING_USER,
-    PASSWORD: process.env.MAILING_PASSWORD,
-  },
   mongo: {
     URL: process.env.MONGO_URL,
-  },
-  jwt: {
-    COOKIE: process.env.JWT_COOKIE,
-    SECRET: process.env.JWT_SECRET,
   },
 };
